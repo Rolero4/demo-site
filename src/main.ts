d5c494d9a62da37e9795ec834d952dae92436308
rolero4 <72434958+Rolero4@users.noreply.github.com>
2024-09-03 13:58:41 +0200

    demo: add vue icons

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { addIcons, OhVueIcon } from "oh-vue-icons";
+import * as FaIcons from "oh-vue-icons/icons/fa";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./styles/tailwind.scss";
 
+const Fa = Object.values({ ...FaIcons });
+addIcons(...Fa);
+
 const app = createApp(App);
+app.component("v-icon", OhVueIcon);
 app.use(router);
 app.mount("#app");
