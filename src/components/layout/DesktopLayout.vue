24520c212affe0877b3036cb71dd02d38d934dea
rolero4 <72434958+Rolero4@users.noreply.github.com>
2024-09-03 13:38:56 +0200

    demo: create ldiffrent layouts for mobile and desktop

--- src/components/layout/DesktopLayout.vue
+++ src/components/layout/DesktopLayout.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
-    desktop
+    <AppHeader></AppHeader>
     <slot></slot>
+    <AppFooter></AppFooter>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import AppHeader from "../core/Header.vue";
+import AppFooter from "../core/MobileMenu.vue";
 
 export default defineComponent({
   name: "AppDesktopLayout",
+  components: {
+    AppHeader,
+    AppFooter,
+  },
 });
 </script>
 
--- src/components/layout/MobileLayout.vue
+++ src/components/layout/MobileLayout.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-    mobile
+    <AppMobileHeader></AppMobileHeader>
     <slot></slot>
+    <AppFooter></AppFooter>
+    <AppMobileMenu></AppMobileMenu>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import AppFooter from "../core/Footer.vue";
+import AppMobileHeader from "../core/MobileHeader.vue";
+import AppMobileMenu from "../core/MobileMenu.vue";
 
 export default defineComponent({
   name: "AppMobileLayout",
+  components: {
+    AppMobileHeader,
+    AppFooter,
+    AppMobileMenu,
+  },
 });
 </script>
 
